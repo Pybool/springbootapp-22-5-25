1f5388c0bbea34f087552bdc4c7d3a7a1197c883
Pybool <ekoemmanueljavl@gmail.com>
2025-05-22 05:11:28 +0100

    first commit

--- walletapp/src/environments/environment.ts
+++ walletapp/src/environments/environment.ts
@@ -8,4 +8,3 @@ export const environment = {
   MAX_INACTIVITY_TIME: 30, //time in minutes
 
 };
-// https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=50&convert=USD&CMC_PRO_API_KEY=c60454ba-c903-411b-aa11-aec5e69bd963
